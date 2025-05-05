fd606a95e26507fde50718aecca48edfdb7d0fef
Dev <dev.bharti@s.amity.edu>
2025-05-05 19:39:35 +0530

    Added metadata and timestamp in branch1

--- File.c
+++ File.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
+#include <time.h>
 int main () {
-	printf("Hello World!");
+	printf("Hello World!\n");
+
+	printf("Program by: Dev\n");
+	time_t now;
+	time(&now);
+	printf("Compiled on: %s", ctime(&now));
+
 	return 0;
 }
