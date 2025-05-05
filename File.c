7c9f02b3d858f2885987c0d8b711e33d59d815ba
Dev <dev.bharti@s.amity.edu>
2025-05-05 20:07:06 +0530

    Added File logging feature in branch3

--- File.c
+++ File.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
+
+void logMessage(const char* msg) {
+	File *file = fopen("log.txt", "a");
+	if(file != NULL) {
+		fprintf(file, "%s\n", msg);
+		fclose(file);
+	}
+}
 int main () {
-	printf("Hello World!");
+	printf("Hello World!\n");
+
+	logMessage("Program started.");
+	logMessage("Displayed Hello World!");
+
 	return 0;
 }
