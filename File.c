e8ec5233aa95d4d368ee64205727fb0bc8be9657
Dev <dev.bharti@s.amity.edu>
2025-05-05 20:14:22 +0530

    Added ASCII banner in branch4

--- File.c
+++ File.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+
+void printAsciiBanner() {
+	printf("\n*****************************\n");
+        printf("*     WELCOME TO GIT LAB    *\n");
+        printf("*****************************\n");
+}
+
 int main () {
+	printAsciiBanner();
 	printf("Hello World!");
+
 	return 0;
 }
