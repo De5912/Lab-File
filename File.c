a28ea88e43cd4cd561d3bfaaa148139b13fc1383
Dev <dev.bharti@s.amity.edu>
2025-05-05 19:54:10 +0530

    Added Interactive menu in branch2

--- File.c
+++ File.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
+
+void showMenu() {
+	printf("\n==== MENU ====\n");
+	printf("1. Say Hello\n");
+	printf("2. Show Info\n");
+	printf("3. Exit\n");
+}
+void optionInfo() {
+	printf("This feature was added in branch2.\n");
+}
+
 int main () {
-	printf("Hello World!");
+	printf("Hello World!\n");
+
+	showMenu();
+	int choice;
+	printf("Enter your choice: ");
+	scanf("%d", &choice);
+
+	switch(choice) {
+		case 1: printf("Hello, User!\n"); break;
+		case 2: optionInfo(); break;
+		case 3: printf("Goodbye!\n"); break;
+		default: printf("Invalid choice.\n");
+	}
 	return 0;
 }
